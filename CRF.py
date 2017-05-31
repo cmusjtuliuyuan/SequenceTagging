55034c16e267b8007722a4f18fc75d8941247f9a
YuanLiu <sjtuliuyuan@gmail.com>
2017-06-01 01:51:51 +0800

    temp

---
+++ CRF.py
@@ -0,0 +1,29 @@
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+# Helper functions to make the code more readable.
+def to_scalar(var):
+    # returns a python float
+    return var.view(-1).data.tolist()[0]
+
+def argmax(vec):
+    # return the argmax as a python int
+    _, idx = torch.max(vec, 1)
+    return to_scalar(idx)
+
+# Compute log sum exp in a numerically stable way for the forward algorithm
+def log_sum_exp(vec):
+    max_score = vec[0, argmax(vec)]
+    max_score_broadcast = max_score.view(1, -1).expand(1, vec.size()[1])
+    return max_score + torch.log(torch.sum(torch.exp(vec - max_score_broadcast)))
+
+class CRF(nn.Module):
+
+	def __init__(self, tagset_size):
+        super(CRF, self).__init__()
+        
+
