12a3ba15727d8a0e3a145e760d2e59b2fac14971
yuan <sjtuliuyuan@gmail.com>
2017-11-28 23:37:04 -0500

    modify helper script

--- run.sh
+++ run.sh
@@ -5,10 +5,22 @@
 # Because there is some bug in my GPU memory
 #
 ####################################
-python main.py
-python main.py --load models/model1.mdl
-python main.py --load models/model2.mdl
-python main.py --load models/model3.mdl
-python main.py --load models/model4.mdl
-python main.py --load models/model5.mdl
-python main.py --load models/model6.mdl
+#python main.py --type supervised --store model/susmodel.mdl
+#ck
+python main.py --type unsupervised --unsupervised_path data/wiki200/wiki200aa.txt --load model/susmodel.mdl --store model/susmodel.mdl
+python main.py --type unsupervised --unsupervised_path data/wiki200/wiki200ab.txt --load model/susmodel.mdl --store model/susmodel.mdl
+python main.py --type unsupervised --unsupervised_path data/wiki200/wiki200ac.txt --load model/susmodel.mdl --store model/susmodel.mdl
+
+
+python main.py --type supervised --load model/susmodel.mdl --store model/model2.mdl
+
+python main.py --type unsupervised --unsupervised_path data/wiki200/wiki200ad.txt --load model/susmodel.mdl --store model/susmodel.mdl
+python main.py --type unsupervised --unsupervised_path data/wiki200/wiki200ae.txt --load model/susmodel.mdl --store model/susmodel.mdl
+python main.py --type unsupervised --unsupervised_path data/wiki200/wiki200af.txt --load model/susmodel.mdl --store model/susmodel.mdl
+python main.py --type unsupervised --unsupervised_path data/wiki200/wiki200ag.txt --load model/susmodel.mdl --store model/susmodel.mdl
+python main.py --type unsupervised --unsupervised_path data/wiki200/wiki200ah.txt --load model/susmodel.mdl --store model/susmodel.mdl
+python main.py --type unsupervised --unsupervised_path data/wiki200/wiki200ai.txt --load model/susmodel.mdl --store model/susmodel.mdl
+python main.py --type unsupervised --unsupervised_path data/wiki200/wiki200aj.txt --load model/susmodel.mdl --store model/susmodel.mdl
+python main.py --type unsupervised --unsupervised_path data/wiki200/wiki200ak.txt --load model/susmodel.mdl --store model/susmodel.mdl
+
+python main.py --type supervised --load model/susmodel.mdl --store model/model6.mdl
--- wiki.py
+++ wiki.py
@@ -9,14 +9,14 @@ def zero_digits(s):
     return re.sub('\d', '0', s)
 
 
-path = 'data/wiki_unlabel'
+path = 'data/wiki100_unlabel'
 files = os.listdir(path)
 for file in files:
 	if file != '.DS_Store':
-		with codecs.open('data/wiki/'+file+'.txt', 'w', 'utf8') as f:
-			for sentence in codecs.open('data/wiki_unlabel/'+file, 'r', 'utf8'):
+		with codecs.open('data/wiki100/'+file+'.txt', 'w', 'utf8') as f:
+			for sentence in codecs.open('data/wiki100_unlabel/'+file, 'r', 'utf8'):
 			    sentence= zero_digits(sentence.rstrip()).split()
-			    if len(sentence) >= 5:
+			    if len(sentence) >= 5 and len(sentence) <= 80:
 			    	for word in sentence:
 			    		f.write(word+'\n')
 			    	f.write('\n')
